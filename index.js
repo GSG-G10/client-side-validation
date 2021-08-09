68b3f12c3cf7d20d4dcf5f08e3842c69e7b42400
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-08-09 23:28:27 +0300

    handle form

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello');
+const form = document.querySelector('#registerForm')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const confirmPassword = document.querySelector('#confirmPassword')
+const submit = document.querySelector('#registerForm button')
+const emailMsg = document.querySelector('#emailMsg')
+const passwordMsg = document.querySelector('#passwordMsg')
+const confirmMsg = document.querySelector('#confirmMsg')
+
+
+const formHandler = () => {
+    const emailVal = email.value
+    if(!email.value || !emailVal.includes('@')) {
+        emailMsg.textContent = 'Please enter a valid email'
+        email.classList.add('err-input');
+    }
+    if (!password.value || password.value.length < 8){
+        passwordMsg.textContent = 'Password should have at least 8 characters'
+        password.classList.add('err-input')
+    }
+    if (confirmPassword.value !== password.value){
+        confirmMsg.textContent = "two passwords doesn't match"
+    }
+}
+form.addEventListener('submit', formHandler)
