24fe3bec19028e4ddb3649bcd4c032ba95c1f1e4
Nizar7zak <nizar7zakout@gmail.com>
2021-08-10 01:27:32 +0300

    solve the challenge

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const form = document.querySelector(".container1");
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#confirmPassword");
+const msg = document.querySelector(".message");
+
+const check = (str) => {
+  const reg =
+    /^(?=.*[0-9])(?=.*[.!@#/$%&*+?^${}()|[\]\\])(?=.*[a-z])(?=.*[A-Z])/;
+  return reg.test(str);
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (password.value !== confirmPassword.value) {
+    return (msg.textContent = "passwords you entered do not match");
+  }
+
+  if (!check(password.value)) {
+    return (msg.textContent = "please write strong password contains Aa#1");
+  }
+
+  return (msg.textContent = "it's okay now  i can send it to server :)");
+});
